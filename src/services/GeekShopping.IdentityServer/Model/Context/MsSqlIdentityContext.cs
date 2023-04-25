dd31eb0e9fb63475532c93909db205d82935daa5
Phillipe Roger Souza <phillrog@hotmail.com>
2023-04-25 20:45:06 -0300

    criação do context

---
+++ src/services/GeekShopping.IdentityServer/Model/Context/MsSqlIdentityContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context
+{
+    public class MsSqlIdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public MsSqlIdentityContext(DbContextOptions<MsSqlIdentityContext> options) : base(options) { }
+    }
+}
