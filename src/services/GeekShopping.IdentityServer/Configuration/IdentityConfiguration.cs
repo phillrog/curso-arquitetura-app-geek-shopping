f46ecd340d898dc568aa0f2ca346d41cd3f23ab4
phillrog <phillrog@hotmail.com>
2023-04-25 20:52:42 -0300

    ajuste nas portas da aplicação web

--- src/services/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ src/services/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -39,8 +39,8 @@ namespace GeekShopping.IdentityServer.Configuration
                     ClientId = "geek_shopping",
                     ClientSecrets = { new Secret("my_super_secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.Code,
-                    RedirectUris = {"https://localhost:4430/signin-oidc"},
-                    PostLogoutRedirectUris = {"https://localhost:4430/signout-callback-oidc"},
+                    RedirectUris = {"https://localhost:57066/signin-oidc"},
+                    PostLogoutRedirectUris = {"https://localhost:57066/signout-callback-oidc"},
                     AllowedScopes = new List<string>
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
