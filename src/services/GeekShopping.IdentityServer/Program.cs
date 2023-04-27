c5e119a4ce85ae9ff8753601bc15db8c6fce312e
Phillipe Roger Souza <phillrog@hotmail.com>
2023-04-27 20:25:01 -0300

    configuração identity

--- src/services/GeekShopping.IdentityServer/Program.cs
+++ src/services/GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,32 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
 var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration["MySQlConnection:Localhost"];
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddEntityFrameworkSqlServer().AddDbContext<MsSqlIdentityContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MsSqlIdentityContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.Events.RaiseFailureEvents = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryClients(IdentityConfiguration.Clients)
+.AddAspNetIdentity<ApplicationUser>()
+.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -17,7 +42,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapRazorPages();
