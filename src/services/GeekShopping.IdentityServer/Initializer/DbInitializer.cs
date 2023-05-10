f15063b702c5e33616c4cc3e2bff49096af8130a
phillrog <phillrog@hotmail.com>
2023-05-10 18:42:17 -0300

    Config inicialzador com dados iniciais

---
+++ src/services/GeekShopping.IdentityServer/Initializer/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace GeekShopping.IdentityServer.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly MsSqlIdentityContext _context;
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly RoleManager<IdentityRole> _role;
+
+        public DbInitializer(MsSqlIdentityContext context,
+            UserManager<ApplicationUser> user,
+            RoleManager<IdentityRole> role)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public void Initialize()
+        {
+           
+        }
+    }
+}
+
