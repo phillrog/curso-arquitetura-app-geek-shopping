7448ca5d6b5d384284a2b2131e8b82514d0739b3
phillrog <phillrog@hotmail.com>
2023-05-15 19:50:03 -0300

    Configuração de autenticação no identity no portal

--- src/web/GeekShopping.Web/Program.cs
+++ src/web/GeekShopping.Web/Program.cs
@@ -1,5 +1,6 @@
 using GeekShopping.Web.Services.IServices;
 using GeekShopping.Web.Services;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,27 @@ builder.Services.AddHttpClient<IProductService, ProductService>(
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+    .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+    .AddOpenIdConnect("oidc", options => 
+    {
+        options.Authority = builder.Configuration["ServiceUrls:IndetityServer"];
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClientId = "geek_shopping";
+        options.ClientSecret = "my_super_secret";
+        options.ResponseType = "code";
+        options.ClaimActions.MapJsonKey("role", "role", "role");
+        options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+        options.Scope.Add("geek_shopping");
+        options.SaveTokens = true;
+    });
 
 var app = builder.Build();
 
@@ -23,10 +45,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
