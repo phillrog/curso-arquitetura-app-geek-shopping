dfe2075988d1c991c774878ae853967173f3ae69
phillrog <phillrog@hotmail.com>
2023-06-15 19:45:58 -0300

    razor page register

---
+++ src/services/GeekShopping.IdentityServer/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeekShopping.IdentityServer.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
