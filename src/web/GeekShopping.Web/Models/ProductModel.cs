33cde7e16f8871ab498c8880caa0a3ffe91ada19
phillrog <phillrog@hotmail.com>
2023-09-16 14:25:24 -0300

    :sparkles: feat: - Finalizando e Testando a View de Detalhes

--- src/web/GeekShopping.Web/Models/ProductModel.cs
+++ src/web/GeekShopping.Web/Models/ProductModel.cs
@@ -1,4 +1,6 @@
-﻿namespace GeekShopping.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeekShopping.Web.Models
 {
     public class ProductModel
     {
@@ -8,6 +10,8 @@
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageURL { get; set; }
+        [Range(1, 100)]
+        public int Count { get; set; }
 
         public string SubstringName() => Name.Length < 24 ? Name : $"{Name.Substring(0, 21)} ...";
         public string SubstringDescription() => Description.Length < 355 ? Name : $"{Name.Substring(0, 352)} ...";
