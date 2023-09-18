c72f72daeddcb9ccd3272906329dad7898d6b8a5
phillrog <phillrog@hotmail.com>
2023-09-18 18:23:04 -0300

    :sparkles: feat: - Foi criado e mapeado entidade CartDetail

---
+++ src/services/GeekShopping.CartAPI/Model/CartDetail.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+
+namespace GeekShopping.CartAPI.Model
+{
+    public class CartDetail : BaseEntity
+    {
+        public int CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public CartHeader CartHeader { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ src/services/GeekShopping.CartAPI/Model/Mappings/CartDetailMapping.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeekShopping.CartAPI.Model.Mappings
+{
+    public class CartDetailMapping : IEntityTypeConfiguration<CartDetail>
+    {
+        public void Configure(EntityTypeBuilder<CartDetail> builder)
+        {
+            builder.ToTable("cart_detail");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.CartHeaderId);
+            builder.Property(x => x.ProductId);
+            builder.Property(x => x.Count);
+
+            builder.HasOne(d => d.Product)
+                .WithMany(d => d.CartDetails)
+                .HasForeignKey(d => d.ProductId);
+
+            builder.HasOne(d => d.CartHeader)
+                .WithMany(d => d.CartDetails)
+                .HasForeignKey(d => d.CartHeaderId);
+        }
+    }
+}
