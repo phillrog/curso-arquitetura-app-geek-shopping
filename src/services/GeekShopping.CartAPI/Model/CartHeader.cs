f4654c2b2f49a70bf3ef27b22d774248a3502f06
phillrog <phillrog@hotmail.com>
2023-09-18 18:22:52 -0300

    :sparkles: feat: - Foi criado e mapeado entidade CartHeader

---
+++ src/services/GeekShopping.CartAPI/Model/CartHeader.cs
@@ -0,0 +1,12 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+
+namespace GeekShopping.CartAPI.Model
+{
+    public class CartHeader : BaseEntity
+    {
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }
+
+        public ICollection<CartDetail> CartDetails { get; set; }
+    }
+}
---
+++ src/services/GeekShopping.CartAPI/Model/Mappings/CartHeaderMapping.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeekShopping.CartAPI.Model.Mappings
+{
+    public class CartHeaderMapping : IEntityTypeConfiguration<CartHeader>
+    {
+        public void Configure(EntityTypeBuilder<CartHeader> builder)
+        {
+            builder.ToTable("cart_header");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.UserId);
+            builder.Property(x => x.CouponCode);
+        }
+    }
+}
