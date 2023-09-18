f57e4089261b9fdfc5b927752ccf6d6b6052a727
phillrog <phillrog@hotmail.com>
2023-09-18 17:30:38 -0300

    :sparkles: feat: - implementado o model e contexto

---
+++ src/services/GeekShopping.CartAPI/Model/Context/CartContext.cs
@@ -0,0 +1,29 @@
+﻿using GeekShopping.CartAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class CartContext : DbContext
+    {
+        public CartContext() {}
+        public CartContext(DbContextOptions<CartContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<Product>().Property(p => p.Id).ValueGeneratedOnAdd();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging();
+        }
+
+    }
+}
