692998163890644e040febd7a6725d4a9606d169
phillrog <phillrog@hotmail.com>
2023-09-18 18:22:19 -0300

    :sparkles: feat: - Foi criado e mapeado entidade Product

---
+++ src/services/GeekShopping.CartAPI/Model/Mappings/ProductMapping.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeekShopping.CartAPI.Model.Mappings
+{
+    public class ProductMapping : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("product");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+            builder.Property(p => p.Name).HasMaxLength(150).IsRequired();
+            builder.Property(p => p.Price).HasPrecision(18,2).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(500);
+            builder.Property(p => p.CategoryName).HasMaxLength(50);
+            builder.Property(p => p.ImageURL).HasMaxLength(300);            
+        }
+    }
+}
