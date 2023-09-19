f7f5ae86c644031f93be4d9af7fcea7f33ce419e
phillrog <phillrog@hotmail.com>
2023-09-19 11:18:28 -0300

    :sparkles: feat: - Iniciando a Implementação das Classes de Repositório

---
+++ src/services/GeekShopping.CartAPI/Repository/CartRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Model.Context;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly CartContext _context;
+        private IMapper _mapper;
+
+        public CartRepository(CartContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> ApplyCoupon(string userId, string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartVO> FindCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveCoupon(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveFromCart(long cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartVO> SaveOrUpdateCart(CartVO vo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
