202c4b52be5094c43cb34d2392215b1c09bcd903
phillrog <phillrog@hotmail.com>
2023-09-19 13:57:16 -0300

    :sparkles: feat: - Implementando o Controller Responsável por Expor os Endpoints de Carrinho

--- src/services/GeekShopping.CartAPI/Program.cs
+++ src/services/GeekShopping.CartAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using GeekShopping.CartAPI.Config;
 using GeekShopping.CartAPI.Model.Context;
+using GeekShopping.CartAPI.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -16,6 +17,9 @@ builder.Services.AddEntityFrameworkSqlServer().AddDbContext<CartContext>(options
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+builder.Services.AddScoped<ICartRepository, CartRepository>();
 
 
 builder.Services.AddControllers();
