5400f5e307d29c85275a291c026b7544df90934a
phillrog <phillrog@hotmail.com>
2023-09-20 19:24:53 -0300

    :sparkles: feat: - Criando os View Models do Carrinho de Compras

---
+++ src/web/GeekShopping.Web/Models/CartDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.Web.Models
+{
+    public class CartDetailViewModel
+    {
+        public int Id { get; set; }
+        public int CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public CartHeaderViewModel CartHeader { get; set; }
+        public ProductViewModel Product { get; set; }
+    }
+}
---
+++ src/web/GeekShopping.Web/Models/CartHeaderViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GeekShopping.Web.Models
+{
+    public class CartHeaderViewModel 
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }
+
+        public ICollection<CartDetailViewModel> CartDetails { get; set; }
+    }
+}
