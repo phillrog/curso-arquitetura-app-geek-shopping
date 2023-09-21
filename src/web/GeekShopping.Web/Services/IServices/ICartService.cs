6dac52274f45cad67568e8c0a026ad071e499ef1
phillrog <phillrog@hotmail.com>
2023-09-21 19:29:00 -0300

    :sparkles: feat:  Definindo as Assinaturas da Interface ICartService

---
+++ src/web/GeekShopping.Web/Services/IServices/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface ICartService
+    {
+        Task<CartViewModel> FindCartByUserId(int userId);
+        Task<CartViewModel> AddItemToCart(CartViewModel cart);
+        Task<CartViewModel> UpdateToCart(CartViewModel cart);
+        Task<bool> RemoveItemFromCart(int cartId);
+        Task<bool> ApplyCoupon(CartViewModel cart, string couponCode);
+        Task<bool> RemoveCoupon(int userId);
+        Task<bool> ClearCart(int userId);
+        Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader);
+    }
+}
