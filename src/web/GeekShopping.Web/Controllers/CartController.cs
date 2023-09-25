b2897a748c2134279074d673db74c7bd4063bcd4
phillrog <phillrog@hotmail.com>
2023-09-25 18:24:29 -0300

    :sparkles: feat: - Adicionando o Carrinho de Compras ao Menu e Criando o CartController

---
+++ src/web/GeekShopping.Web/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        public async Task<IActionResult> CartIndex()
+        {
+            return View();
+        }
+    }
+}
