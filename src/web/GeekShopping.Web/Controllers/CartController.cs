74500e9c7eef83c92e4c1c96c5c10736bf3b7398
phillrog <phillrog@hotmail.com>
2023-09-25 18:25:19 -0300

    :sparkles: feat: - Adicionando serviços ao CartController

--- src/web/GeekShopping.Web/Controllers/CartController.cs
+++ src/web/GeekShopping.Web/Controllers/CartController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ILogger<CartController> _logger;
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(ILogger<CartController> logger, IProductService productService, ICartService cartService)
+        {
+            _logger = logger;
+            _productService = productService;
+            _cartService = cartService;
+        }
         public async Task<IActionResult> CartIndex()
         {
             return View();
