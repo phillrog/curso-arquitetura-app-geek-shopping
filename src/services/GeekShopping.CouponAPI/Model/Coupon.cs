b12a9f80697d094ac3a5b7e08d8a4c008beb9f97
phillrog <phillrog@hotmail.com>
2023-09-30 15:38:39 -0300

    :sparkles: feat: - Criação da entidade coupon

---
+++ src/services/GeekShopping.CouponAPI/Model/Coupon.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekShopping.CouponAPI.Model
+{
+    public class Coupon 
+    {
+        public int Id { get; set; }
+        public string CouponCode { get; set; }
+        public decimal DiscountAmount { get; set; }
+    }
+}
