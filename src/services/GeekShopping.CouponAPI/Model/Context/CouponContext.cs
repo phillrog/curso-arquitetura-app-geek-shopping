60499a4f0c112bee9d21f4416948a982c8a5f3fd
phillrog <phillrog@hotmail.com>
2023-10-01 14:19:23 -0300

    :sparkles: feat: - Criação do contexto coupon

---
+++ src/services/GeekShopping.CouponAPI/Model/Context/CouponContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CouponAPI.Model.Context
+{
+    public class CouponContext : DbContext
+    {
+        public CouponContext() {}
+        public CouponContext(DbContextOptions<CouponContext> options) : base(options) {}
+
+        public DbSet<Coupon> Coupon { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CouponContext).Assembly);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging();
+        }
+
+    }
+}
