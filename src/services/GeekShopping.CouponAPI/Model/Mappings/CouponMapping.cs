3cacaaf29e1328aee16bf696d04976db33ff4d39
phillrog <phillrog@hotmail.com>
2023-10-01 14:19:55 -0300

    :sparkles: feat: - Criado o mapping

---
+++ src/services/GeekShopping.CouponAPI/Model/Mappings/CouponMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeekShopping.CouponAPI.Model.Mappings
+{
+    public class CouponMapping : IEntityTypeConfiguration<Coupon>
+    {
+        public void Configure(EntityTypeBuilder<Coupon> builder)
+        {
+            builder.ToTable("coupon");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+            builder.Property(p => p.CouponCode).HasColumnName("coupon_name").HasMaxLength(30).IsRequired();
+            builder.Property(p => p.DiscountAmount).HasColumnName("discount_amount").HasPrecision(18,2).IsRequired();
+            
+        }
+    }
+}
