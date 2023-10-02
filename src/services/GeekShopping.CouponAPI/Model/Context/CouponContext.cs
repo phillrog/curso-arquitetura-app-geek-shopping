da5ab49a0fb5b1f8ff83d27cfaaba10d15c17bfc
phillrog <phillrog@hotmail.com>
2023-10-02 19:21:32 -0300

    :sparkles: feat: - Config  no context coupon modelbuilder

--- src/services/GeekShopping.CouponAPI/Model/Context/CouponContext.cs
+++ src/services/GeekShopping.CouponAPI/Model/Context/CouponContext.cs
@@ -11,9 +11,24 @@ namespace GeekShopping.CouponAPI.Model.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             foreach (var relationship in modelBuilder.Model.GetEntityTypes()
                 .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(CouponContext).Assembly);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon { 
+                Id = 1, 
+                CouponCode = "DIA_DAS_CRIANCAS_40",
+                DiscountAmount = 40
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                Id = 2,
+                CouponCode = "BLACK_FRIDAY_75",
+                DiscountAmount = 75
+            });
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
