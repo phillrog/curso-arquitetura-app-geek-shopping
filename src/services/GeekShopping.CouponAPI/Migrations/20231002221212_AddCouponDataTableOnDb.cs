b830d3471dfb8d49a2ee02b51f541cb1484a6c5f
phillrog <phillrog@hotmail.com>
2023-10-02 19:20:27 -0300

    :sparkles: feat: - Criação da migration da tabela coupon

---
+++ src/services/GeekShopping.CouponAPI/Migrations/20231002221212_AddCouponDataTableOnDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCouponDataTableOnDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "coupon",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    coupon_name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    discount_amount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_coupon", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "coupon");
+        }
+    }
+}
