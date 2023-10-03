292f217040c24c4386d8ffdf8f37707df3e450c3
phillrog <phillrog@hotmail.com>
2023-10-03 19:12:35 -0300

    :sparkles: feat: - Criação e implementação do controller coupon e getcoupon

---
+++ src/services/GeekShopping.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,28 @@
+﻿using GeekShopping.CouponAPI.Data.ValueObjects;
+using GeekShopping.CouponAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private ICouponRepository _repository;
+
+        public CouponController(ICouponRepository repository)
+        {
+            _repository = repository ?? throw new
+                ArgumentNullException(nameof(repository));
+        }
+
+
+        [HttpGet("{couponCode}")]
+        public async Task<ActionResult<CouponVO>> FindById(string couponCode)
+        {
+            var product = await _repository.GetCouponByCouponCode(couponCode);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+    }
+}
