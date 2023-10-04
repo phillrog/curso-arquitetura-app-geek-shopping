1e06a0533b15c24a57e3ae4dbee6cfd1296d0e9e
phillrog <phillrog@hotmail.com>
2023-10-04 19:51:00 -0300

    :sparkles: feat: - Consumindo o Microsserviço de Cupons de Desconto (GeekShopping.CouponAPI)

---
+++ src/web/GeekShopping.Web/Models/CouponViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekShopping.Web.Models
+{
+    public class CouponViewModel
+    {
+        public int Id { get; set; }
+        public string CouponCode { get; set; }
+        public decimal DiscountAmount { get; set; }
+    }
+}
--- src/web/GeekShopping.Web/Services/CouponService.cs
+++ src/web/GeekShopping.Web/Services/CouponService.cs
@@ -1,8 +1,26 @@
-﻿using GeekShopping.Web.Services.IServices;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Utils;
+using System.Net;
 
 namespace GeekShopping.Web.Services
 {
     public class CouponService : ICouponService
     {
+        private readonly HttpClient _client;
+        public const string BasePath = "api/v1/coupon";
+
+        public CouponService(HttpClient client)
+        {
+            _client = client;
+        }
+        public async Task<CouponViewModel> GetCoupon(string code)
+        {
+            var response = await _client.GetAsync($"{BasePath}/api/{code}");
+
+            if (response.StatusCode != HttpStatusCode.OK) return new CouponViewModel();
+
+            return await response.ReadContentAs<CouponViewModel>();
+        }
     }
 }
