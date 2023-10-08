2260a2b8c8a7aef53adbea58bb7938d74638086a
phillrog <phillrog@hotmail.com>
2023-10-08 14:05:52 -0300

    :sparkles: feat - Criação da mensagem básica

---
+++ src/integration/GeekShopping.MessageBus/BaseMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace GeekShopping.MessageBus
+{
+    public abstract class BaseMessage
+    {
+        public int Id { get; set; }
+        public DateTime MessageCreated { get; set; }
+    }
+}
