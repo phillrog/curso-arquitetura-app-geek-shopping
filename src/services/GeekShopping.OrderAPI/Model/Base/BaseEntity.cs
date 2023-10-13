53feba262c486c1b9d6f0e91550e28b882ace6f2
phillrog <phillrog@hotmail.com>
2023-10-13 16:40:56 -0300

    :sparkles: feat: Criação das entidades

---
+++ src/services/GeekShopping.OrderAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.OrderAPI.Model
+{
+    public class BaseEntity
+    {
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+    }
+}
---
+++ src/services/GeekShopping.OrderAPI/Model/OrderDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.OrderAPI.Model
+{
+    public class OrderDetail : BaseEntity
+    {
+        public int OrderHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
