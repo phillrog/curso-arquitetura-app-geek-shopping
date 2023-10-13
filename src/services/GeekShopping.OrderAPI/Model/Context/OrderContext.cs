316b9fee3ff6929f60e19dc44bf51e51b0f31b46
phillrog <phillrog@hotmail.com>
2023-10-13 16:40:35 -0300

    :sparkles: feat: Criação do contexto de order

---
+++ src/services/GeekShopping.OrderAPI/Model/Context/OrderContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Model.Context
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext() {}
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) {}
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrderContext).Assembly);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging();
+        }
+
+    }
+}
