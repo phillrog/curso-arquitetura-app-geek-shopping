4cc032a2b52dd4ccd47a2529819c2f273b48283d
phillrog <phillrog@hotmail.com>
2023-10-24 19:03:00 -0300

    Adicionando os VO's Necessários ao Consumo do GeekShopping.CouponAPI

---
+++ src/services/GeekShopping.CartAPI/Repository/CouponRepository.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly CartContext _context;
+       
+        public async Task<CouponVO> GetCouponByCouponCode(string couponCode)
+        {
+            var coupon = await _context.Coupon.FirstOrDefaultAsync(a => a.CouponCode == couponCode);
+            return _mapper.Map<CouponVO>(coupon);
+        }
+    }
+}
