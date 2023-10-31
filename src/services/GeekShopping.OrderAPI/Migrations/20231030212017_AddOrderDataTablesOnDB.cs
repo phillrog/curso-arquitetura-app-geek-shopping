38a9acae19c1d44fc728c3142850c00e348248d8
phillrog <phillrog@hotmail.com>
2023-10-31 18:41:26 -0300

    Migrations da api order tabelas

---
+++ src/services/GeekShopping.OrderAPI/Migrations/20231030212017_AddOrderDataTablesOnDB.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.OrderAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderDataTablesOnDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "order_header",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    coupon_code = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    purchase_amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    discount_amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    purchase_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    phone_number = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    card_number = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CVV = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    expiry_month_year = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    total_itens = table.Column<int>(type: "int", nullable: false),
+                    order_time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PaymentStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order_header", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_detail",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderHeaderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    product_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order_detail", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_order_detail_order_header_OrderHeaderId",
+                        column: x => x.OrderHeaderId,
+                        principalTable: "order_header",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_detail_OrderHeaderId",
+                table: "order_detail",
+                column: "OrderHeaderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "order_detail");
+
+            migrationBuilder.DropTable(
+                name: "order_header");
+        }
+    }
+}
